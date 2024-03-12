4ff34487eef3ce2428a0d59c399e260e54ce5072
ArulMuruganSivapoosam <46676192+Arulmurugan-Sivapoosam@users.noreply.github.com>
2024-03-12 13:14:20 +0530

    style: chart bubble color updated.

--- Source/Charts/Renderers/LineChartRenderer.swift
+++ Source/Charts/Renderers/LineChartRenderer.swift
@@ -666,7 +666,7 @@ open class LineChartRenderer: LineRadarRenderer
                     let latency: TimeInterval = isFirstLoading ? 0.5 : 0
                     DispatchQueue.main.asyncAfter(deadline: .now() + latency) { [weak self] in
                         guard let self else {return}
-                        self.bubbleAnimationView?.addPulseEffect(at: .zero, with: dataSet.getCircleColor(atIndex: _xBounds.max) ?? .white, size: rect.size.height)
+                        self.bubbleAnimationView?.addPulseEffect(at: .zero, with: dataSet.fillColor, size: rect.size.height)
                         UIView.animate(withDuration: 1, delay: 0, options: [.autoreverse, .repeat], animations: {
                             self.bubbleAnimationView?.transform = CGAffineTransform(translationX: 0, y: -2)
                         }, completion: nil)
